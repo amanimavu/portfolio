e35b926833e97dd9b694a666d324e3dc44deef86
amanimavu <amanimavu@gmail.com>
2025-04-30 10:36:32 +0300

    feat: update Navbar component to accept pathname prop and apply dynamic styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,35 +1,39 @@
 import { Link } from 'gatsby'
-import React from 'react'
+import React, { CSSProperties, useMemo } from 'react'
 
-export function Navbar() {
-    return (
-        <div className="navigation-bar">
-            <nav
-                aria-label="Top navigation"
-                className="navigation-bar-container"
-            >
-                <Link
-                    className="navigation-bar__item"
-                    activeClassName="link-active"
-                    to="/"
-                >
+export function Navbar({ pathname }: { pathname: string }) {
+    const navBarStyles = useMemo(() => {
+        const styles: CSSProperties = {}
+        if (!['/', '/about-me/'].includes(pathname)) {
+            styles['backdropFilter'] = 'blur(10px)'
+        }
+        return styles
+    }, [pathname])
+
+    const linkStyles = useMemo(() => {
+        const styles: CSSProperties = {}
+        if (pathname === '/about-me/') {
+            styles['color'] = '#f6f6f6'
+        }
+        return styles
+    }, [pathname])
+    return !pathname.includes('blog') ? (
+        <div className="navigation-bar" style={navBarStyles}>
+            <nav aria-label="Top navigation" className="navigation-bar-container">
+                <Link className="navigation-bar__item" activeClassName="link-active" to="/about-me" style={linkStyles}>
                     me
                 </Link>
-                <Link
-                    className="navigation-bar__item"
-                    activeClassName="link-active"
-                    to="/blogs"
-                >
+                <Link className="navigation-bar__item" activeClassName="link-active" to="/blogs" style={linkStyles}>
                     blogs
                 </Link>
-                <Link
-                    className="navigation-bar__item"
-                    activeClassName="link-active"
-                    to="/#portfolio"
-                >
+                <Link className="navigation-bar__item" activeClassName="link-active" to="/portfolio" style={linkStyles}>
                     portfolio
                 </Link>
             </nav>
+        </div>
+    ) : (
+        <div className="brand-container">
+            <h2>MAVU</h2>
         </div>
     )
 }
