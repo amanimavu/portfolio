4fb11a938f37d503983948e4cc9f8dbc5cf26faa
amanimavu <amanimavu@gmail.com>
2025-04-30 10:37:29 +0300

    refactor: simplify Index component by removing Level1Template and updating image paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,34 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
 import type { HeadFC, PageProps } from 'gatsby'
-import { Level1Template } from 'templates/level-1'
-import favicon from '../images/favicon.png'
+import favicon from '../images/pngs/favicon.png'
 import '../styles.css'
 import { StaticImage } from 'gatsby-plugin-image'
 
-const IndexPage: React.FC<PageProps> = () => {
+export default function Index(props: PageProps) {
     return (
-        <Level1Template>
+        <div id="landing-page">
             <StaticImage
                 className="hero-image"
-                src="../images/hero-image.png"
+                src="../images/pngs/hero-image.png"
                 alt="hero image"
                 layout="constrained"
                 width={500}
                 placeholder="blurred"
             />
-        </Level1Template>
+        </div>
     )
 }
-
-export default IndexPage
 
 export const Head: HeadFC = () => {
     return (
         <>
-            <title>Amani Mavu</title>
+            <title id="title">Amani Mavu</title>
             <meta
+                id="viewport"
                 name="viewport"
                 content="width=device-width, initial-scale=1"
             />
-            <link rel="icon" href={favicon} />
+            <link id="icon" rel="icon" href={favicon} />
         </>
     )
 }
