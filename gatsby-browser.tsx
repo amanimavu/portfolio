26d6fec15f73cd66130377b2876ca0b983ca2d0d
amanimavu <amanimavu@gmail.com>
2025-06-10 10:59:05 +0300

    feat: mount BlogsLayout when under the blogs route segment

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import type { GatsbyBrowser } from 'gatsby'
-import { RootLayout } from 'components/root-layout'
+import React from "react"
+import type { GatsbyBrowser } from "gatsby"
+import { RootLayout } from "components/root-layout"
+import { BlogsLayout } from "components/blogs-layout"
 
-export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
-    element,
-    props,
-}) => {
+export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({ element, props }) => {
     return (
         <>
-            <RootLayout {...props}>{element}</RootLayout>
+            <RootLayout {...props}>
+                {props.location.pathname.includes("blogs") ? <BlogsLayout {...props}>{element}</BlogsLayout> : element}
+            </RootLayout>
         </>
     )
 }
