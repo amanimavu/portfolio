ce3ab9d91d17f2b18f49d8df5894f42409f54970
amanimavu <amanimavu@gmail.com>
2025-08-22 13:43:05 +0300

    feat: update about-me page with new SVG icons and enhance styling
    - Added new SVG icons for JavaScript, TypeScript, React, Next.js, PHP, and MySQL.
    - Updated the about-me page to include a section displaying the tools of trade.
    - Enhanced CSS styles for better layout and responsiveness, including animations for tool icons.
    - Adjusted font sizes and line heights for improved readability.
    - Introduced new keyframe animations for elements on the page.

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -2,20 +2,35 @@ import * as React from "react"
 import type { HeadFC } from "gatsby"
 import { AboutTemplate } from "templates/about"
 import { SEO } from "components/seo"
+import { ReactComponent as JavaScriptIcon } from "images/svgs/tools/javascript.svg"
+import { ReactComponent as TypescriptIcon } from "images/svgs/tools/typescript.svg"
+import { ReactComponent as ReactIcon } from "images/svgs/tools/react.svg"
+import { ReactComponent as NextIcon } from "images/svgs/tools/nextjs.svg"
+import { ReactComponent as PhpIcon } from "images/svgs/tools/php.svg"
+import { ReactComponent as MysqlIcon } from "images/svgs/tools/mysql.svg"
+import { useScreens } from "utils/hooks"
 
 export default function AboutMe() {
+    const [md] =useScreens()
     return (
         <AboutTemplate>
             <p>
                 I'm Amani Mavu, a self-taught software developer with a background in applied mathematics. My passion
                 for problem-solving led me to front-end development, where I specialize in building intuitive and
-                high-performing web applications using JavaScript, TypeScript, and React. I have experience working in
-                fintech and developing POS and finance-related systems. I'm always eager to tackle new challenges,
-                optimize workflows, and stay ahead in the ever-evolving tech landscape. Currently, I’m looking for
-                opportunities that will push my engineering and problem-solving skills to the next level while expanding
-                into full-stack development. When I’m not coding, you’ll probably find me watching an episode of Shark
-                Tank and getting inspired by innovative business ideas.
+                high-performing web applications.
             </p>
+            <p>
+                I am just to leave this here <mark>amanimavu@gmail.com</mark>
+            </p>
+
+            {!md ? <section id="tools-of-trade">
+                <JavaScriptIcon id="javascript" className="icon" />
+                <TypescriptIcon id="typescript" className="icon" />
+                <ReactIcon id="react" className="icon" />
+                <NextIcon id="next" className="icon" />
+                <PhpIcon id="php" className="icon" />
+                <MysqlIcon id="mysql" className="icon" />
+            </section>: null}
         </AboutTemplate>
     )
 }
