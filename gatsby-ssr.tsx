134f293ae7d87e09afc7098c6718c6e80407f144
amanimavu <amanimavu@gmail.com>
2026-02-19 14:18:36 +0300

    refactor: update theme management
    - Prevent the Flash of Unstyled Content (FOUC), by injecting a script into the HTML header during the build process using Gatsby's Server-Side Rendering (SSR) APIs

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,12 +1,47 @@
 import React from "react"
 import { RootLayout } from "components/root-layout"
-import { GatsbyBrowser } from "gatsby"
+import { GatsbySSR } from "gatsby"
 import { BlogsLayout } from "components/blogs-layout"
 
-export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({ element, props }) => {
+export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({ element, props }) => {
     return (
         <RootLayout {...props}>
             {props.location.pathname.includes("blogs") ? <BlogsLayout {...props}>{element}</BlogsLayout> : element}
         </RootLayout>
     )
+}
+
+export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }, options) => {
+    setHeadComponents([
+        <script
+            key="tailwind-dark-mode"
+            dangerouslySetInnerHTML={{
+                __html: `
+                (function() {
+                    try {
+                        const theme = localStorage.getItem('theme');
+                        const supportDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches === true;
+                        if (!theme && supportDarkMode) theme = 'dark';
+                        
+                        if (theme === 'dark') {
+                            document.documentElement.setAttribute("data-theme", "dark");
+                        } else {
+                            document.documentElement.setAttribute("data-theme", "light");
+                        }
+                    } catch (e) {}
+                })();
+                (function() {
+                    try {
+                        const hasGoodNetwork = ["4g", "3g"].includes(navigator.connection?.effectiveType ?? "2g");
+                        const saveDate = navigator.connection?.saveData ?? false
+
+                        if(!hasGoodNetwork || saveData){
+                            document.documentElement.style.setProperty("--mask-url", "none")
+                        }
+                    } catch (e) {}
+                })();
+            `,
+            }}
+        />,
+    ])
 }
